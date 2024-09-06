786bb2f0e0f375fd32263e06d434338f285debe0
Blesso <wmugo@kabarak.ac.ke>
2024-09-06 21:56:55 +0300

    commit

---
+++ if-else.dart
@@ -0,0 +1,9 @@
+void main() {
+  var age = 20;
+  if (age > 18) {
+    print("Mariam is a voter in Kenya");
+  } 
+  else {
+    print("Mariam is still young to vote");
+  }
+}
---
+++ if.dart
@@ -0,0 +1,7 @@
+void main() {
+  var age = 30;
+  if (age > 18) {
+    print("Mariam is a voter in Kenya");
+  }
+  //print("Mariam is still young to vote");
+}
